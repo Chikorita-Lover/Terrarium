1a84c7034f8a13fde0e856cdba5318dbe1492c3b
Chikorita Lover <cjgardels@gmail.com>
2023-03-15 11:52:25 -0500

    Update initial commit

---
+++ src/main/java/com/chikoritalover/terrarium/Terrarium.java
@@ -0,0 +1,14 @@
+package com.chikoritalover.terrarium;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Terrarium implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("terrarium");
+	public static final String MODID = "terrarium";
+
+	@Override
+	public void onInitialize() {}
+}
--- src/main/java/com/chikoritalover/terrarium/TerrariumDataGenerator.java
+++ src/main/java/com/chikoritalover/terrarium/TerrariumDataGenerator.java
@@ -1,9 +1,9 @@
-package com.chikoritalover;
+package com.chikoritalover.terrarium;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+public class TerrariumDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
